637abc6bf21cbe3a3af06b2a91be7b75fe263349
Gaozhongpai <gaozhongpai@gmail.com>
2022-03-18 17:26:56 -0400

    add xnnpack check

---
+++ 04_Linux_Installation_pai/main.cpp
@@ -0,0 +1,12 @@
+#include <tensorflow/lite/c/c_api.h>
+#include <tensorflow/lite/delegates/xnnpack/xnnpack_delegate.h>
+
+int main()
+{
+    auto m_model = TfLiteModelCreateFromFile("../../models/model.fp32.tflite");
+    auto m_options = TfLiteInterpreterOptionsCreate();
+    TfLiteXNNPackDelegateOptions opt = TfLiteXNNPackDelegateOptionsDefault();
+    auto m_xnnpack_delegate = TfLiteXNNPackDelegateCreate(&opt);
+    TfLiteInterpreterOptionsAddDelegate(m_options, m_xnnpack_delegate);
+    auto m_interpreter = TfLiteInterpreterCreate(m_model, m_options); // This returns nullptr and prints error messages in the console
+}
